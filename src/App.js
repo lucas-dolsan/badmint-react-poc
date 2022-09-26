438f6e49f0a88163f9b8e9635cf60e9a59d2989d
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2022-09-25 23:20:06 -0300

    feat(Router): react router demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,103 @@
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Badmint Frontend React POC</h1>
-        <h2>UDESC - CEAVI</h2>
-        <h3>GRP II</h3>
-      </header>
+    <div>
+      <h1>Exemplo de rotas</h1>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/not-found">Not found</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  const [count, setCount] = React.useState(0);
+
+  function cliquei() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <h2>Dashboard</h2>
+      <button style={{ width: "5rem" }} onClick={cliquei}>
+        teste
+      </button>
+      <h3>{count}</h3>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        flexDirection: "column",
+      }}
+    >
+      <h2>404 NOT FOUND</h2>
+      <p>
+        <Link to="/">Homepage</Link>
+      </p>
+    </div>
+  );
+}
